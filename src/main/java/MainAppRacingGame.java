e736b89d7b09da6997b02329c5dff51a91faa519
itdar <thesockerrr@gmail.com>
2021-05-02 23:40:43 +0900

    refactor: 프로그램 및 레이싱게임 시작 부분 인풋 메서드 이름과 위치 리팩토링

--- src/main/java/MainAppRacingGame.java
+++ src/main/java/MainAppRacingGame.java
@@ -4,9 +4,7 @@ public class MainAppRacingGame {
 
     public static void main(String[] args) {
         RacingGameController gameController = new RacingGameController();
-        gameController.initCars();
-        gameController.initTryCount();
-        System.out.println("\n실행 결과");
+        gameController.initializeRacingGame();
         gameController.startGame();
     }
 
--- src/main/java/controller/RacingGameController.java
+++ src/main/java/controller/RacingGameController.java
@@ -7,8 +7,6 @@ import packaging.RaceCount;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.List;
-import java.util.Random;
 import java.util.Scanner;
 
 public class RacingGameController {
@@ -19,11 +17,16 @@ public class RacingGameController {
     public RacingGameController() { }
 
     public void startGame() {
+        printRacingStart();
         while (raceCount.hasNextRound()) {
             cars.race();
             printProgress();
         }
         printResult(cars.getChampions());
+    }
+
+    private void printRacingStart() {
+        System.out.println("\n실행 결과");
     }
 
     private void printProgress() {
@@ -34,7 +37,12 @@ public class RacingGameController {
         System.out.println(champions.namesWithComma() + "(이)가 최종 우승 했습니다.");
     }
 
-    public void initCars() {
+    public void initializeRacingGame() {
+        initCars();
+        initTryCount();
+    }
+
+    private void initCars() {
         try {
             System.out.println("경주할 자동차 이름을 입력하세요. (이름은 쉼포(,) 기준으로 구분)");
             cars = new Cars(inputCarNames());
@@ -48,7 +56,7 @@ public class RacingGameController {
         return reader.readLine().split(",");
     }
 
-    public void initTryCount() {
+    private void initTryCount() {
         System.out.println("시도할 회수는 몇회인가요?");
         Scanner scanner = new Scanner(System.in);
         raceCount = new RaceCount(scanner.nextInt());
